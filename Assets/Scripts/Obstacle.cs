5926c5c150ff37a11002483d9bb4fb58b7d1847d
Sean Kirkpatrick <sean@buildproto.com>
2015-07-11 16:35:55 -0400

    added IRecycle interface, and Obstacle script to generate random sprite obstacle

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour, IRecycle {
+
+	public Sprite[] sprites;
+
+	public void Restart() {
+		var renderer = GetComponent<SpriteRenderer> ();
+		renderer.sprite = sprites [Random.Range (0, sprites.Length)];
+	}
+
+	public void Shutdown() {
+
+	}
+}
