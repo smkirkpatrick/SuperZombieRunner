2a9444d470ed48235ca18699ff23cc6da52859bb
Sean Kirkpatrick <sean@buildproto.com>
2015-07-11 16:01:06 -0400

    initial commit, has a scrolling environment and semi-randomly generated obstacles that clean up after themselves and recycle objects

---
+++ Assets/Scripts/DestroyOffscreen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOffscreen : MonoBehaviour {
+
+	public float offset = 16f; // Overridden by the Object that the script is attached to.
+
+	private bool offscreen;
+	private float offscreenX = 0;
+	private Rigidbody2D body2d;
+
+	void Awake() {
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		//Debug.LogFormat ("Screen width: {0}, pixelToUnits: {1}, offset: {2}", Screen.width, PixelPerfectCamera.pixelsToUnits, offset);
+		offscreenX = ((Screen.width / PixelPerfectCamera.pixelsToUnits) / 2) + offset;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var posX = transform.position.x;
+		var dirX = body2d.velocity.x;
+
+		if (Mathf.Abs (posX) > offscreenX) {
+
+			if (dirX < 0 && posX < -offscreenX) {
+				//Debug.LogFormat ("Moving left, posX: {0} vs offscreenX: {1}", posX, -offscreenX);
+				offscreen = true;
+			} else if (dirX > 0 && posX > offscreenX) {
+				//Debug.LogFormat ("Moving right, posX: {0} vs offscreenX: {1}", posX, offscreenX);
+				offscreen = true;
+			}
+		} else {
+			offscreen = false;
+		}
+
+		if (offscreen) {
+			//Debug.Log ("Destroying offscreen object");
+			OnOutOfBounds();
+		}
+	}
+
+	public void OnOutOfBounds() {
+		offscreen = false;
+		GameObjectUtil.Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/PixelPerfectCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelPerfectCamera : MonoBehaviour {
+
+	public static float pixelsToUnits = 1f;
+	public static float scale = 1f;
+
+	public Vector2 nativeResolution = new Vector2 (240, 160);
+
+	// Use this to run logic before Start()
+	void Awake () {
+		var camera = GetComponent<Camera> ();
+
+		if (camera.orthographic) {
+			scale = Screen.height / nativeResolution.y;
+			pixelsToUnits *= scale;
+			camera.orthographicSize = Mathf.Ceil ((Screen.height / 2.0f) / pixelsToUnits);
+			Debug.Log (string.Format ("Screen height: {0}, scale: {1}, pixelsToUnits: {2}, camera scaled: {3}",
+			                          Screen.height, scale, pixelsToUnits, camera.orthographicSize));
+		}
+	}
+}
