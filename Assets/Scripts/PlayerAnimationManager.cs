0b0801d5fb5e8d9456c13fb8bfa7cf04a2dbafee
Sean Kirkpatrick <sean@buildproto.com>
2015-07-15 14:20:21 -0400

    created PlayerAnimationManager to control state of animation transition bool Running

---
+++ Assets/Scripts/PlayerAnimationManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationManager : MonoBehaviour {
+
+	private Animator animator;
+	private InputState inputState;
+
+	void Awake() {
+		animator = GetComponent<Animator> ();
+		inputState = GetComponent<InputState> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		var running = true;
+		// ^ Default state of the player
+
+		if (inputState.absVelocityX > 0 && inputState.absVelocityY < inputState.standingThreshold) {
+			running = false;
+		}
+
+		animator.SetBool ("Running", running);
+	}
+}
