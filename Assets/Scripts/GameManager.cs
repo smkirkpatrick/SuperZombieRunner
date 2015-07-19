ce90dbb6c5c15064879f9a437fb9fead61a30524
Sean Kirkpatrick <sean@buildproto.com>
2015-07-19 10:19:30 -0400

    added ResetGame method to GameManager that instantiates an instance of the Player prefab when invoked - and deleted Player GameObject from the scene because now it is generated by the GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	public GameObject floor;
-	public Spawner spawner;
+	public GameObject playerPrefab;
+
+	private GameObject player;
+	private GameObject floor;
+	private Spawner spawner;
 
 	void Awake() {
 		floor = GameObject.Find ("Foreground");
@@ -20,10 +23,18 @@ public class GameManager : MonoBehaviour {
 		floor.transform.position = pos;
 
 		spawner.active = false;
+
+		ResetGame ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void ResetGame() {
+		spawner.active = true;
+
+		player = GameObjectUtil.Instantiate (playerPrefab, new Vector3 (0, (Screen.height / PixelPerfectCamera.pixelsToUnits) / 2, 0));
 	}
 }
